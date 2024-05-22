05919f292595a363fb55d98835b7d757db4aa49e
EpicET <etowner@middlebury.edu>
2024-05-21 22:19:17 -0400

    Add BERT

---
+++ BERT.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+from transformers import AutoModel
+
+class ApplicationReviewModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.bert =  AutoModel.from_pretrained('distilbert-base-uncased')
+        self.linear_layer = nn.Linear(768, 5)
+        
+
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        cls_embedding = outputs.last_hidden_state[:, 0, :]  
+        logits = self.linear_layer(cls_embedding)
+        return logits 
+
